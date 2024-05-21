a5bb4b7b7f524368b2e94c3d744a695feb4968af
kayque <kayquebraga@outlook.com>
2024-05-20 23:20:14 -0300

    Configuração inicial do projeto

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Meu Projeto");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+}
